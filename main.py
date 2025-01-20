b9047600311b5fca9a5f088f784037d7e3206c52
antijun <davidwu361@gmail.com>
2025-01-19 23:10:07 -0500

    delete unused files

--- main.py
+++ main.py
@@ -89,5 +89,5 @@ def main(img_path):
     print(f"Translated Image Saved to: {output_text_overlay_path}")
 
 if __name__ == "__main__":
-    img_path = "test_panels/mushoku21.jpg"
+    img_path = "test_panels/mushoku3.jpg"
     main(img_path)
